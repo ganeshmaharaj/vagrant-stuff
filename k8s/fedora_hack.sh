4b4a3272c3e1ffbec3e16a0c88c9c40dae7a033d
Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
2020-08-24 16:31:05 -0700

    Add Fedora 32
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>

--- k8s/fedora_hack.sh
+++ k8s/fedora_hack.sh
@@ -2,7 +2,17 @@
 
 set -o errexit
 set -o nounset
+set -o xtrace
 
 echo "Disabling firewalld..."
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+
+source /etc/os-release
+
+if [ "${VERSION_ID}" == "32" ]; then
+	sudo dnf install -y grubby
+	sudo grubby \
+	  --update-kernel=ALL \
+	  --args="systemd.unified_cgroup_hierarchy=0"
+fi
