5935d3034ba35510e3f9c93b903ad64d814f0cce
Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
2022-09-08 10:36:35 -0700

    Distro update
    Update to latest versions of distros and clean up fedora cgroups v1
    provision
    
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>

--- k8s/fedora_hack.sh
+++ k8s/fedora_hack.sh
@@ -8,12 +8,3 @@ echo "Disabling firewalld..."
 if $(sudo systemctl is-active firewalld > /dev/null); then
   sudo systemctl disable --now firewalld
 fi
-
-source /etc/os-release
-
-if [ "${VERSION_ID}" == "32" ]; then
-	sudo dnf install -y grubby
-	sudo grubby \
-	  --update-kernel=ALL \
-	  --args="systemd.unified_cgroup_hierarchy=0"
-fi
